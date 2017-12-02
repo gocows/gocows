ea93533903dd0f235081749516e2842bd06055b7
Taras Shavarovskyi <taras.shavarovskyy@everymatrix.com>
2017-12-02 14:57:55 +0200

    Added suncalc

--- metspider/js/app.js
+++ metspider/js/app.js
@@ -22,7 +22,7 @@ var MetSpider = function() {
 	];
 	
 	var timestamp2Time = function(timestamp) {
-		return new Date(timestamp*1000).toTimeString();
+		return new Date(timestamp*1000).toLocaleTimeString();
 	};
 	
 	var timestamp2Date = function(timestamp) {
@@ -75,6 +75,7 @@ var MetSpider = function() {
 				var hourlyTemp = [];
 				var hourlyDewPoint = [];
 				var hourlyPressure = [];
+				var hourlyWindBearing = [];
 				var hourlyWindSpeed = [];
 				var hourlyHumidity = [];
 				var hourlyWindGust = [];
@@ -84,24 +85,60 @@ var MetSpider = function() {
 				var hourlyPrecipProbability = [];
 				var hourlyOzone = [];
 				var hourlyCloudCover = [];
+				var xPlotBands = [];
 				for (var index in data.hourly.data) {
-					xAxisLabels[index] = timestamp2Date(data.hourly.data[index].time).substring(0, 4) + ' ' + timestamp2Time(data.hourly.data[index].time).substring(0, 5);
-					hourlyTemp[index] = data.hourly.data[index].temperature;
-					hourlyHumidity[index] = data.hourly.data[index].humidity*100;
-					hourlyWindSpeed[index] = [data.hourly.data[index].windSpeed, data.hourly.data[index].windBearing];
-					hourlyPressure[index] = data.hourly.data[index].pressure;
-					hourlyDewPoint[index] = data.hourly.data[index].dewPoint;
-					hourlyWindGust[index] = data.hourly.data[index].windGust;
-					hourlyVisibility[index] = data.hourly.data[index].visibility;
-					hourlyUvIndex[index] = data.hourly.data[index].uvIndex;
-					hourlyPrecipIntensity[index] = data.hourly.data[index].precipIntensity;
-					hourlyPrecipProbability[index] = data.hourly.data[index].precipProbability*100;
-					hourlyOzone[index] = data.hourly.data[index].ozone;//
-					hourlyCloudCover[index] = data.hourly.data[index].cloudCover*100;
+					var timestamp = data.hourly.data[index].time*1000;
+					xAxisLabels[index] = data.hourly.data[index].time*1000;//timestamp2Date(data.hourly.data[index].time).substring(0, 4) + ' ' + timestamp2Time(data.hourly.data[index].time).substring(0, 5);
+					hourlyTemp[index] = [timestamp, data.hourly.data[index].temperature];
+					hourlyHumidity[index] = [timestamp, data.hourly.data[index].humidity*100];
+					hourlyWindSpeed[index] = [timestamp, data.hourly.data[index].windSpeed];
+					hourlyWindBearing[index] = [timestamp, data.hourly.data[index].windSpeed, data.hourly.data[index].windBearing];
+					hourlyPressure[index] = [timestamp, data.hourly.data[index].pressure];
+					hourlyDewPoint[index] = [timestamp, data.hourly.data[index].dewPoint];
+					hourlyWindGust[index] = [timestamp, data.hourly.data[index].windGust];
+					hourlyVisibility[index] = [timestamp, data.hourly.data[index].visibility];
+					hourlyUvIndex[index] = [timestamp, data.hourly.data[index].uvIndex];
+					var pIntens = data.hourly.data[index].precipIntensity;
+					var pProb = data.hourly.data[index].precipProbability*100;
+					hourlyPrecipIntensity[index] = [timestamp, pProb < 6 || pIntens < 0.06 ? 0 : pIntens];
+					hourlyPrecipProbability[index] = [timestamp, pProb < 6 || pIntens < 0.06 ? 0 : pProb];
+					hourlyOzone[index] = [timestamp, data.hourly.data[index].ozone];
+					hourlyCloudCover[index] = [timestamp, data.hourly.data[index].cloudCover*100];
 				}
-				console.log(hourlyWindSpeed);
+				var minDate = new Date(data.daily.data[0].time*1000);
+				var maxDate = new Date(data.daily.data[data.daily.data.length-1].time*1000+24*60*60*1000);
+				var xPlotBands = [];
+				var tmpDate = minDate;
+				var prevSunset = SunCalc.getTimes(new Date(minDate.getTime() - 1000*60*60*24), place.lat, place.lon).sunset;
+				while (tmpDate <= maxDate) {
+					var times = SunCalc.getTimes(tmpDate, place.lat, place.lon);
+					xPlotBands[xPlotBands.length] = {
+					    from: prevSunset,
+					    to: times.sunrise,
+					    color: 'rgba(158, 170, 113, 0.1)'
+					};
+					xPlotBands[xPlotBands.length] = {
+					    from: times.sunrise,
+					    to: times.sunset,
+					label: {
+						text: times.sunrise.toString().substring(0, 3),
+						style: {
+						    color: '#606060'
+						}
+					    }
+					};
+					tmpDate = new Date(tmpDate.getTime() + 1000*60*60*24);
+					prevSunset = times.sunset;
+				}
+				console.log(xPlotBands);
+				
 
 				$('#chartContainer').show();
+				Highcharts.setOptions({
+				    global: {
+					useUTC: false
+				    }
+				});
 				Highcharts.chart('chartContainer', {
 					title: {
 						text: 'Forecast in ' + place.name + ' (' + provider.name + ')',
@@ -117,9 +154,10 @@ var MetSpider = function() {
 						}
 					},
 					xAxis: [{
-						categories: xAxisLabels,
+						type: 'datetime',
 						crosshair: true,
-						offset: 40
+						offset: 40,
+						plotBands: xPlotBands
 					}],
 					yAxis: [{
 						labels: {
@@ -133,7 +171,91 @@ var MetSpider = function() {
 							style: {
 								color: Highcharts.getOptions().colors[1]
 							}
-						}
+						},
+						plotLines: [{
+							value: 0,
+							color: 'blue',
+							dashStyle: 'dot',
+							label : {
+								text: '0°C'
+							},
+							width: 3
+						},
+						{
+							value: -5,
+							color: 'blue',
+							dashStyle: 'dot',
+							width: 2
+						},
+						{
+							value: -10,
+							color: 'blue',
+							dashStyle: 'dot',
+							label : {
+								text: '-10°C'
+							},
+							width: 2
+						},
+						{
+							value: -15,
+							color: 'blue',
+							dashStyle: 'dot',
+							width: 2
+						},
+						{
+							value: -20,
+							color: 'blue',
+							dashStyle: 'dot',
+							label : {
+								text: '-20°C'
+							},
+							width: 2
+						},
+						{
+							value: 5,
+							color: 'red',
+							dashStyle: 'dot',
+							width: 2
+						},
+						{
+							value: 10,
+							color: 'red',
+							dashStyle: 'dot',
+							label : {
+								text: '10°C'
+							},
+							width: 2
+						},
+						{
+							value: 15,
+							color: 'red',
+							dashStyle: 'dot',
+							width: 2
+						},
+						{
+							value: 20,
+							color: 'red',
+							dashStyle: 'dot',
+							label : {
+								text: '20°C'
+							},
+							width: 2
+						},
+						{
+							value: 25,
+							color: 'red',
+							dashStyle: 'dot',
+							width: 2
+						},
+						{
+							value: 30,
+							color: 'red',
+							dashStyle: 'dot',
+							label : {
+								text: '30°C'
+							},
+							width: 2
+						}]
 					},
 					{
 						labels: {
@@ -147,7 +269,13 @@ var MetSpider = function() {
 							style: {
 								color: Highcharts.getOptions().colors[1]
 							}
-						}
+						},
+						plotLines: [{
+							value: 1013.25,
+							color: 'red',
+							dashStyle: 'solid',
+							width: 1
+						}]
 					},
 					{
 						title: {
@@ -305,22 +433,34 @@ var MetSpider = function() {
 					{
 						name: 'Wind Bearing',
 						type: 'windbarb',
-						data: hourlyWindSpeed,
+						data: hourlyWindBearing,
 						visible: false,
 						tooltip: {
 							valueSuffix: ' m/s'
-						}
+						},
+						onSeries: 'wind-speed-series'
 					},
 					{
 						name: 'Wind Speed',
 						yAxis: 3,
-						keys: ['y', 'rotation'],
 						type: 'area',
 						data: hourlyWindSpeed,
 						visible: false,
+						id: 'wind-speed-series',
 						tooltip: {
 							valueSuffix: ' m/s'
-						}
+						},
+						 fillColor: {
+						    linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+						    stops: [
+							[0, Highcharts.getOptions().colors[0]],
+							[
+							    1,
+							    Highcharts.color(Highcharts.getOptions().colors[0])
+								.setOpacity(0.1).get()
+							]
+						    ]
+						},
 					},
 					{
 						name: 'Wind Gust',
